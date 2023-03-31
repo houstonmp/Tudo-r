6c89e74f8fb4ef46682a84b2b3d33d958aa98317
Matthew Houston <matthouston027@gmail.com>
2023-03-31 21:59:09 +0900

    Fix Error where image wasn't deploying

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,10 +1,11 @@
+import logo from '../../assets/Tudor_rose.png'
 import './Nav.css';
 
 const Navbar = () => {
 
     return (<nav>
         <span>
-            <img src="src\assets\Tudor_rose.png"></img>
+            <img src={logo}></img>
         </span>
         <h1>Tudo[r]</h1>
     </nav>
