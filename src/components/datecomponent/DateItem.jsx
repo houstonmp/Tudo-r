f3c6fbfa0de2a434df11cf8dfc142856a8304966
Matthew Houston <matthouston027@gmail.com>
2023-03-31 12:31:00 +0900

    Finish fixing colors to match the Figma design

--- src/components/datecomponent/DateItem.jsx
+++ src/components/datecomponent/DateItem.jsx
@@ -2,7 +2,7 @@ import styles from './DateItem.module.css'
 
 const DateItem = () => {
     return (
-        <div className={styles.date}>
+        <div className={`${styles.date} ${styles.active}`}>
             <h4 className={`${styles['date-text']}`}>Jan</h4>
             <h4 className={`${styles['date-text']}`}>26</h4>
         </div >
--- src/components/datecomponent/DateManager.jsx
+++ src/components/datecomponent/DateManager.jsx
@@ -1,9 +1,13 @@
 import DateItem from './DateItem';
+// import Form from './Form'
+
 import styles from './DateManager.module.css';
+
 
 const DateManager = () => {
     return (
         <article className={`${styles['date-man']}`}>
+            {/* <Form /> */}
             <DateItem />
             <DateItem />
             <DateItem />
