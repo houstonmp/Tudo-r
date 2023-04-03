6155a18111ed95c2052f914ce01964c0883bfe50
Matthew Houston <matthouston027@gmail.com>
2023-04-04 04:13:54 +0900

    Add Working arrow buttons to control dates

--- src/components/datecomponent/DateManager.jsx
+++ src/components/datecomponent/DateManager.jsx
@@ -6,7 +6,7 @@ import DateSelect from './DateSelect';
 
 import styles from './DateManager.module.css';
 
-const TODAYS_DATE = new Date(2023, 0, 28);
+const TODAYS_DATE = new Date();
 
 
 const DateManager = () => {
@@ -17,6 +17,62 @@ const DateManager = () => {
         date: TODAYS_DATE.getDate()
     });
 
+
+    // const [displayArrow, setArrow] = useState({
+    //     left: false,
+    //     right: false
+    // });
+
+    let displayArrow = {
+        left: false,
+        right: true
+    }
+
+    let startDate = 0;
+    let endDate = 5;
+
+    const setDates = () => {
+        if (filterDate.date < 5) {
+            startDate = dateList[0].getDate();
+            if (1 === filterDate.date) {
+                displayArrow = {
+                    left: false,
+                    right: true
+                }
+            } else {
+                displayArrow = {
+                    left: true,
+                    right: true
+                }
+            }
+
+        }
+        else if (filterDate.date >= 5 && filterDate.date < (dateList.length - 3)) {
+            startDate = filterDate.date - 2;
+            endDate = filterDate.date + 2;
+            displayArrow = {
+                left: true,
+                right: true
+            }
+        }
+        else if (filterDate.date > (dateList.length - 6)) {
+            startDate = dateList[dateList.length - 5].getDate();
+            endDate = dateList[dateList.length - 1].getDate();
+            if (dateList.length === filterDate.date) {
+                displayArrow = {
+                    left: true,
+                    right: false
+                }
+            } else {
+                displayArrow = {
+                    left: true,
+                    right: true
+                }
+            }
+
+        }
+    }
+
     const initDateArr = (month, year) => {
         let date = new Date(year, month, 1);
         let days = [];
@@ -24,8 +80,11 @@ const DateManager = () => {
             days.push(new Date(date));
             date.setDate(date.getDate() + 1);
         }
+
         return days;
     }
+
+    const dateList = initDateArr(filterDate.month, filterDate.year);
 
     const initOptions = () => {
         const currentYear = parseInt(TODAYS_DATE.getFullYear());
@@ -36,8 +95,6 @@ const DateManager = () => {
         return yearArr;
     }
 
-    const dateList = initDateArr(filterDate.month, filterDate.year);
-
     const monthHandler = (newMonth) => {
         setDate((prevState) => {
             return {
@@ -46,7 +103,6 @@ const DateManager = () => {
                 date: 1
             }
         })
-        // setDateManager();
     }
 
     const yearHandler = (newYear) => {
@@ -57,9 +113,9 @@ const DateManager = () => {
                 date: 1
             }
         })
-        // setDateManager();
     }
 
+    setDates();
 
     const options = {
         years: initOptions(),
@@ -79,51 +135,63 @@ const DateManager = () => {
         ]
     };
 
-    let startDate = 0;
-    let endDate = 5;
-
-    const setDates = () => {
-        if (filterDate.date < 5) {
-            startDate = dateList[0].getDate();
-        }
-        else if (filterDate.date >= 5 && filterDate.date < (dateList.length - 3)) {
-            startDate = filterDate.date - 2;
-            endDate = filterDate.date + 3;
-        }
-        else if (filterDate.date > (dateList.length - 6)) {
-            console.log("DateList:", dateList[dateList.length - 5].getDate(), dateList[dateList.length - 1].getDate());
-            startDate = dateList[dateList.length - 5].getDate();
-            endDate = dateList[dateList.length - 1].getDate();
-        }
+    const moveLeft = () => {
+        setDate((prevState) => {
+            return {
+                month: prevState.month,
+                year: prevState.year,
+                date: (prevState.date - 1)
+            }
+        })
+        setDates();
+    }
+    const moveRight = () => {
+        setDate((prevState) => {
+            console.log(prevState + 1);
+            return {
+                month: prevState.month,
+                year: prevState.year,
+                date: (prevState.date + 1)
+            }
+        })
+        setDates();
     }
 
-    setDates();
+
+
+
 
     const filterArr = dateList.filter((el) => {
         return el.getDate() >= startDate && el.getDate() <= endDate;
     })
 
     return (
-        <div className='datePicker'>
+        <div className='datePicker' onLoad={setDates}>
             <div className="filter">
                 <p>Filter by:</p>
                 <DateSelect valueType="month" value={filterDate.month} options={options.months} onSaveDate={monthHandler} />
                 <DateSelect valueType="year" value={filterDate.year} options={options.years} onSaveDate={yearHandler} />
             </div>
             <article className={`${styles['date-man']}`}>
+                {displayArrow.left && <span className="material-symbols-outlined" onClick={moveLeft}>
+                    arrow_left
+                </span>}
                 {/* <Form /> */}
                 {
+
                     filterArr.map((el, index) => {
                         if (filterDate.date === el.getDate()) {
-                            console.log("FilterDate:", filterDate.date, index)
                             return <DateItem key={`${el.getDate()}-${filterDate.month}`} active="active" month={options.months[filterDate.month].slice(0, 3)} date={el.getDate()} />
                         }
                         else {
-                            console.log("OtherDate:", filterDate.date, index)
                             return <DateItem key={`${el.getDate()}-${filterDate.month}`} month={options.months[filterDate.month].slice(0, 3)} date={el.getDate()} />
                         }
                     })
                 }
+                {displayArrow.right && <span className="material-symbols-outlined" onClick={moveRight}>
+                    arrow_right
+                </span>}
+
             </article >
         </div >
     )
