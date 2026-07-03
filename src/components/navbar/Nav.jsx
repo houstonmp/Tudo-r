02295bd04f1fae63d24425af53eeb3650e3a7758
Matthew Houston <matthouston027@gmail.com>
2026-07-03 02:33:23 -0500

    Address css styling issue in build

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -4,7 +4,7 @@ import styles from './Nav.module.css';
 const Navbar = () => {
 
     return (<nav>
-        <img src={logo}></img>
+        <img src={logo} className={styles.logo}></img>
         <h1 className="junge-default">Tudo[r]</h1>
         <h2>Task management fit for the royal court</h2>
     </nav>
